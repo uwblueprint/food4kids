1002eaa4691033189dfad6dad32204f9b204ecfe
ludavidca <davidjylu7@gmail.com>
2025-10-05 16:30:45 -0400

    Fixed Lint Issues

--- backend/python/app/models/driver_history.py
+++ backend/python/app/models/driver_history.py
@@ -8,9 +8,13 @@ from .base import BaseModel
 # if TYPE_CHECKING:
 #     from .driver import Driver
 
+
 class DriverHistoryBase(SQLModel):
     """Shared fields between table and API models"""
-    driver_id: int = Field(foreign_key="drivers.id", index=True)  # TODO FK to driver table, to validate later
+
+    driver_id: int = Field(
+        foreign_key="drivers.id", index=True
+    )  # TODO FK to driver table, to validate later
     year: int = Field(nullable=False)
     km: float = Field(nullable=False)
 
@@ -24,22 +28,27 @@ class DriverHistoryBase(SQLModel):
 
 class DriverHistory(DriverHistoryBase, BaseModel, table=True):
     """Database table model"""
+
     __tablename__ = "driver_history"
     driver_history_id: int | None = Field(default=None, primary_key=True)
     # driver: "Driver" = Relationship(back_populates="history")
 
+
 class DriverHistoryCreate(DriverHistoryBase):
     """Create request model"""
+
     pass
 
 
 class DriverHistoryRead(DriverHistoryBase):
     """Read response model"""
+
     driver_history_id: int
 
 
 class DriverHistoryUpdate(SQLModel):
     """Update request model - all optional"""
+
     driver_id: int | None = None
     year: int | None = None
     km: float | None = None
