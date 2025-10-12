fd9f3dae0a2a88dcdf61dcb62aabe4ef7aea02b8
Kerushani Sivaneswaran <99515111+Kerushani@users.noreply.github.com>
2025-10-11 20:30:10 -0400

    [F4KRP-85] Create jobs table
    [F4KRP-85] Create jobs table (#12)

---
+++ backend/python/app/models/jobs.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlmodel import Field, SQLModel
+
+from .base import BaseModel
+from .enum import ProgressEnum
+
+
+class JobBase(SQLModel):
+    """Shared fields between table and API models"""
+
+    route_group_id: UUID | None = Field(foreign_key="route_groups.route_group_id")
+    progress: ProgressEnum = Field(default=ProgressEnum.PENDING)
+
+
+class Job(JobBase, BaseModel, table=True):
+    """Job model for demonstration purposes"""
+
+    __tablename__ = "job"
+
+    job_id: UUID = Field(default_factory=uuid4, primary_key=True)
+    started_at: datetime | None = Field(
+        default=None,
+    )
+    updated_at: datetime | None = Field(
+        default=None,
+    )
+    finished_at: datetime | None = Field(
+        default=None,
+    )
+
+
+class JobCreate(JobBase):
+    """Job creation request"""
+
+    pass
+
+
+class JobRead(JobBase):
+    """Job response model"""
+
+    job_id: UUID
+
+
+class JobUpdate(SQLModel):
+    """Job update request - all optional"""
+
+    progress: ProgressEnum | None = None
+    route_group_id: UUID | None = None
+    started_at: datetime | None = None
+    updated_at: datetime | None = None
+    finished_at: datetime | None = None
