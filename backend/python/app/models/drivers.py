de2b55e452f38b01b8c669ef68ab2f1b3d6a2ab1
PranavGopinath <pranavgopinath1209@gmail.com>
2025-10-12 13:19:54 -0400

    use emailStr for drivers

--- backend/python/app/models/drivers.py
+++ backend/python/app/models/drivers.py
@@ -1,5 +1,6 @@
 from uuid import UUID, uuid4
 
+from pydantic import EmailStr
 from sqlmodel import Field, SQLModel
 
 from .base import BaseModel
@@ -7,7 +8,7 @@ from .base import BaseModel
 
 class DriverBase(SQLModel):
     name: str = Field(min_length=1, max_length=255)
-    email: str = Field(min_length=1, max_length=255)
+    email: EmailStr = Field(unique=True, index=True, max_length=254)
     phone: str = Field(min_length=1, max_length=20)
     address: str = Field(min_length=1, max_length=255)
     license_plate: str = Field(min_length=1, max_length=20)
@@ -32,7 +33,7 @@ class DriverRead(DriverBase):
 
 class DriverUpdate(SQLModel):
     name: str | None = Field(default=None, min_length=1, max_length=255)
-    email: str | None = Field(default=None, min_length=1, max_length=255)
+    email: EmailStr | None = Field(default=None, max_length=254)
     phone: str | None = Field(default=None, min_length=1, max_length=20)
     address: str | None = Field(default=None, min_length=1, max_length=255)
     license_plate: str | None = Field(default=None, min_length=1, max_length=20)
