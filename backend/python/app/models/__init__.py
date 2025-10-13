27f9f91253bdac9a6c3ce61d475313ccab2ccdd4
Kerushani Sivaneswaran <99515111+Kerushani@users.noreply.github.com>
2025-10-12 20:15:13 -0400

    Fix job import

--- backend/python/app/models/__init__.py
+++ backend/python/app/models/__init__.py
@@ -83,7 +83,8 @@ def init_app(_app: Any | None = None) -> None:
     from .driver_history import DriverHistory  # noqa: F401
     from .driver_assignment import DriverAssignment  # noqa: F401
     from .entity import Entity  # noqa: F401
-    from .jobs import Jobs  # noqa: F401
+    from .location_group import LocationGroup  # noqa: F401
+    from .job import Job  # noqa: F401
     from .simple_entity import SimpleEntity  # noqa: F401
     from .user import User  # noqa: F401
 
