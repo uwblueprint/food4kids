1e644701063b85a3177be7dd956d32dadde87224
ChloeW125 <139719503+ChloeW125@users.noreply.github.com>
2025-10-13 14:43:27 -0400

    create admin info table (#13)
    * admin info table
    
    * fixed formatting and linting issues
    
    * implemented all code review comments
    
    * Merge branch 'database-schemas' into chloe/admin-info-table
    
    * fixed formatting and linting issues
    
    * Merge branch 'database-schemas' into chloe/admin-info-table
    
    * fix linting and formatting
    
    * Merge branch 'database-schemas' into chloe/admin-info-table
    
    * fix formating errors

--- backend/python/app/models/__init__.py
+++ backend/python/app/models/__init__.py
@@ -90,10 +90,8 @@ def init_app(_app: Any | None = None) -> None:
     from .location_group import LocationGroup  # noqa: F401
     from .route_stops import RouteStop  # noqa: F401
     from .routes import Route  # noqa: F401
-    from .location_group import LocationGroup  # noqa: F401
-    from .jobs import Job  # noqa: F401
-    from .location_group import LocationGroup  # noqa: F401
     from .job import Job  # noqa: F401
+    from .location_group import LocationGroup  # noqa: F401
     from .simple_entity import SimpleEntity  # noqa: F401
     from .user import User  # noqa: F401
 
