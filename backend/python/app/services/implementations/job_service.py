0569227489a7ecae405db3c5d3246fab9bf16dfb
Kerushani Sivaneswaran <99515111+Kerushani@users.noreply.github.com>
2025-10-26 17:06:02 -0400

    run formatter

--- backend/python/app/services/implementations/job_service.py
+++ backend/python/app/services/implementations/job_service.py
@@ -8,14 +8,19 @@ from app.models.enum import ProgressEnum
 =======
 >>>>>>> 653b6fc (add route to get all routes)
 from app.models.job import Job
+from app.services.interfaces.job_service import IJobService
 
 
+<<<<<<< HEAD
 class JobService:
 <<<<<<< HEAD
     """Modern FastAPI-style job service"""
 
 =======
 >>>>>>> 653b6fc (add route to get all routes)
+=======
+class JobService(IJobService):
+>>>>>>> f642f68 (run formatter)
     def __init__(self, logger: logging.Logger, session: AsyncSession):
         self.logger = logger
         self.session = session
@@ -32,7 +37,11 @@ class JobService:
             statement = statement.where(Job.progress == progress)
         result = await self.session.execute(statement)
 <<<<<<< HEAD
+<<<<<<< HEAD
         return list(result.scalars().all())
 =======
         return result.scalars().all()
 >>>>>>> 653b6fc (add route to get all routes)
+=======
+        return result.scalars().all()
+>>>>>>> f642f68 (run formatter)
