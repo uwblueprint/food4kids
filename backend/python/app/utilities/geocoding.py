a63df3d0327d16ad8e4cff01199cee292dbdac43
Hy Lac <hylacnguyen2006@gmail.com>
2025-10-29 12:07:22 -0400

    Update function description

--- backend/python/app/utilities/geocoding.py
+++ backend/python/app/utilities/geocoding.py
@@ -20,16 +20,17 @@ async def geocode(address: str) -> dict[str, float] | None:
         return None
 
 
-# Accepts a list of strings representing addresses
-# Returns a list of {"lat": ..., "lng": ...} one to one for each address
-# If address is invalid, there will be None instead
 async def geocode_addresses(addresses: list[str]) -> list[dict[str, float] | None]:
+    """
+    Accepts a list of strings representing addresses
+    Returns a list of {"lat": ..., "lng": ...} one to one for each address
+    If address is invalid, there will be None instead
+
+    Example Usage:
+    test = ["200 University Ave West, Waterloo, Ontario", "InvalidAddress"]
+    results = asyncio.run(geocode_addresses(test))
+    print(results)
+    Resulting output: [{'lat': 43.4729399, 'lng': -80.54007159999999}, None]
+    """
     tasks = [geocode(address) for address in addresses]
     return await asyncio.gather(*tasks)
-
-
-# test = ["200 University Ave West, Waterloo, Ontario", "InvalidAddress"]
-# results = asyncio.run(geocode_addresses(test))
-# print(results)
-
-# Resulting output: [{'lat': 43.4729399, 'lng': -80.54007159999999}, None]
