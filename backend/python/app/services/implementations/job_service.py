2d5d4cb6bdbf100e12b9540a0caf2f0dbf9230af
Kerushani Sivaneswaran <99515111+Kerushani@users.noreply.github.com>
2025-11-02 17:56:58 -0500

    create inital skeleton for generation service

--- backend/python/app/services/implementations/job_service.py
+++ backend/python/app/services/implementations/job_service.py
@@ -1,4 +1,5 @@
 import logging
+from uuid import UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select
@@ -6,11 +7,15 @@ from sqlmodel import select
 <<<<<<< HEAD
 <<<<<<< HEAD
 from app.models.enum import ProgressEnum
+<<<<<<< HEAD
 =======
 >>>>>>> 653b6fc (add route to get all routes)
 =======
 from app.models.enum import ProgressEnum
 >>>>>>> 31f7f05 (add copiolet suggestions)
+=======
+from app.models.generation_type import RouteGenerationRequest
+>>>>>>> 8fdc839 (create inital skeleton for generation service)
 from app.models.job import Job
 from app.services.interfaces.job_service import IJobService
 
@@ -48,6 +53,7 @@ class JobService(IJobService):
 <<<<<<< HEAD
 <<<<<<< HEAD
         return list(result.scalars().all())
+<<<<<<< HEAD
 =======
         return result.scalars().all()
 >>>>>>> 653b6fc (add route to get all routes)
@@ -57,3 +63,16 @@ class JobService(IJobService):
 =======
         return list(result.scalars().all())
 >>>>>>> 7c99254 (fix linter errors)
+=======
+
+    async def create_generation_job(self, req: RouteGenerationRequest) -> Job:
+        job = Job(progress=ProgressEnum.PENDING, payload=req.model_dump())
+        self.session.add(job)
+        await self.session.commit()
+        await self.session.refresh(job)
+        return job
+
+    async def get_job(self, job_id: UUID) -> Job | None:
+        result = await self.session.execute(select(Job).where(Job.id == job_id))
+        return result.scalar_one_or_none()
+>>>>>>> 8fdc839 (create inital skeleton for generation service)
