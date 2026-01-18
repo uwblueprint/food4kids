2183148e38ceee5c7d1607551dbed75fd600fd01
ludavidca <davidjylu7@gmail.com>
2026-01-18 18:48:16 -0500

    Migrated to pnpm

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
-    open: true,
-  },
-  plugins: [react()],
+    host: true, // Important for Docker
+  }
 })
