5ff6080f056f7ec7b14c06e1dcc9a2a920b43baf
ludavidca <davidjylu7@gmail.com>
2026-02-01 23:49:48 -0500

    Fix Lint

--- backend/python/app/dependencies/auth.py
+++ backend/python/app/dependencies/auth.py
@@ -58,7 +58,7 @@ def require_authorization_by_role(roles: set[str]) -> Callable:
         session: AsyncSession = Depends(get_session),
     ) -> bool:
         try:
-            #TODO: Fix Authorization and Role Based Management in future commit.
+            # TODO: Fix Authorization and Role Based Management in future commit.
 
             # authorized = await auth_service.is_authorized_by_role(
             #     session, access_token, roles
@@ -68,7 +68,7 @@ def require_authorization_by_role(roles: set[str]) -> Callable:
             #         status_code=status.HTTP_401_UNAUTHORIZED,
             #         detail="You are not authorized to make this request.",
             #     )
-            
+
             return True
         except Exception as e:
             raise HTTPException(
