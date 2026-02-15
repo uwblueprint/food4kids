6d552f69e3b0af45292a67963e0d6bdbf2a4e71a
Hy Lac <hylacnguyen2006@gmail.com>
2026-02-14 20:18:12 -0500

    add update_user_role to firebase script

--- backend/python/app/update_firebase.py
+++ backend/python/app/update_firebase.py
@@ -1,8 +1,11 @@
+import os
+
 from firebase_admin import auth
 
 from app import initialize_firebase
+from app.models.enum import RoleEnum
 
-admin_email = "food4kids@uwblueprint.org"
+admin_email = os.getenv("ADMIN_EMAIL")
 
 
 def update_all_users_role(role_name: str) -> None:
@@ -10,7 +13,6 @@ def update_all_users_role(role_name: str) -> None:
     Iterates through all Firebase users and sets a custom 'role' claim.
     Always ensures the admin only has the role 'admin'
     """
-    initialize_firebase()
     print(f"Starting update: Setting all non admin users to role: {role_name}")
 
     # List all users (paginated)
@@ -37,8 +39,23 @@ def update_all_users_role(role_name: str) -> None:
     print(f"\nSuccessfully updated {count} users.")
 
 
+def update_user_role(user_email: str, role: str) -> None:
+    """
+    Update the firebase role of a user given their email
+    NOTE: This assumes that your local db has the correct roles set
+    """
+    try:
+        print(f"Updating {user_email} to have {role} role")
+        user = auth.get_user_by_email(user_email)
+        auth.set_custom_user_claims(user.uid, {"role": role})
+    except auth.UserNotFoundError:
+        print(f"Error: No user found with email {user_email}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
 if __name__ == "__main__":
-    # Change to new desired role
-    # NOTE: This overwrites preexisting roles so be careful!
-    new_role = "driver"
-    update_all_users_role(new_role)
+    initialize_firebase()
+    # This should only be used to tweak firebase to match your local db
+    your_email = ""  # Update as necessary
+    update_user_role(your_email, RoleEnum.DRIVER)
