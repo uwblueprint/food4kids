485b5c79eba966a4acff352a8f8c72a05a6f3bd4
Landon Trinh <landon.trinh@icloud.com>
2026-03-07 19:23:24 -0500

    feat: fallback defaults for halal and boxes fields

--- backend/python/app/models/location_group.py
+++ backend/python/app/models/location_group.py
@@ -3,8 +3,6 @@ from uuid import UUID, uuid4
 
 from sqlmodel import Field, Relationship, SQLModel
 
-from app.models.location import LocationRead
-
 from .base import BaseModel
 
 # temporary import to avoid circular dependency
@@ -45,7 +43,6 @@ class LocationGroupRead(LocationGroupBase):
 
     location_group_id: UUID
     num_locations: int
-    locations: list[LocationRead]
 
 
 class LocationGroupUpdate(SQLModel):
