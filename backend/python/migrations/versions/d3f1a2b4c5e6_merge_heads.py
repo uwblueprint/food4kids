19fed0ddd1bb1b6048e694328b6d58ea63a2174c
ludavidca <davidjylu7@gmail.com>
2026-04-07 19:29:36 -0400

    Fix Duplicate

---
+++ backend/python/migrations/versions/d3f1a2b4c5e6_merge_heads.py
@@ -0,0 +1,21 @@
+"""merge heads
+
+Revision ID: d3f1a2b4c5e6
+Revises: 692d84fc21be, 595032f5ede7
+Create Date: 2026-04-07 00:00:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd3f1a2b4c5e6'
+down_revision = ('692d84fc21be', '595032f5ede7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
