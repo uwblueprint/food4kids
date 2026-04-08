fd2cc72ff2d6b0f3fcee3e1122bcbeef6f8f2ac7
Hy Lac <hylacnguyen2006@gmail.com>
2026-04-07 21:49:51 -0400

    lint + format

--- backend/python/tests/test_models.py
+++ backend/python/tests/test_models.py
@@ -19,7 +19,6 @@ from app.models.announcement import (
 from app.models.driver import (
     Driver,
     DriverCreate,
-    DriverRegister,
     DriverUpdate,
 )
 from app.models.driver_assignment import (
@@ -155,7 +154,7 @@ class TestCoreBusinessValidation:
         # Test valid password
         user_finalize = UserFinalize(
             user_invite_id=uuid4(),
-            password="securepassword123",            
+            password="securepassword123",
         )
         assert user_finalize.password == "securepassword123"
 
