fff0f684abd8e10ff6b47094e7ca09a727533bd3
Landon Trinh <landon.trinh@icloud.com>
2026-04-07 20:16:40 -0400

    feat: component barrel file

--- frontend/src/common/components/index.ts
+++ frontend/src/common/components/index.ts
@@ -1,6 +1,8 @@
 // Export common components here as they are created
 // Example: export { Button } from './Button';
 // Example: export { Modal } from './Modal';
+export { Badge } from './Badge';
+export { Banner } from './Banner';
 export { Button } from './Button';
 export { Dropdown } from './Dropdown';
 export { FilterChip, FilterChipGroup } from './FilterChip';
