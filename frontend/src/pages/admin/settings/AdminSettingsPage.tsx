d777bd90868145dfd9829a61c9a5807e93b348ab
Landon Trinh <landon.trinh@icloud.com>
2026-04-28 13:13:02 -0400

    feat: setup settings page

--- frontend/src/pages/admin/settings/AdminSettingsPage.tsx
+++ frontend/src/pages/admin/settings/AdminSettingsPage.tsx
@@ -1,5 +1,25 @@
-import { PlaceholderPage } from '../../../common/components/PlaceHolderPage';
+import { Account, Button } from '@/common/components';
+import MegaphoneIcon from '@/assets/icons/megaphone.svg?react';
+import SearchIcon from '@/assets/icons/search.svg?react';
 
 export const AdminSettingsPage = () => {
-  return <PlaceholderPage pageName="Admin Settings Page" />;
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex items-start justify-between">
+        <h1>Settings</h1>
+        <div className="flex items-center gap-6">
+          <div className="flex items-center gap-4">
+            <Button variant="tertiary" shape="circular">
+              <MegaphoneIcon className="size-5 text-blue-300" />
+            </Button>
+            <Button variant="tertiary" shape="circular">
+              <SearchIcon className="size-5 text-blue-300" />
+            </Button>
+          </div>
+          <Account />
+        </div>
+      </div>
+      <div>TODO</div>
+    </div>
+  );
 };
