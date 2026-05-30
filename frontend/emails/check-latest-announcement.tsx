5de803e1be18eed2440a00556b3e689b8c0bc82b
ChloeW125 <139719503+ChloeW125@users.noreply.github.com>
2026-05-30 10:39:52 -0400

    Templates using react email (#121)
    * Setup React Email (code + dependencies) System and Email Templates
    
    * Extracted Tailwind Config to js with a quick script
    
    * Merge branch 'main' of https://github.com/uwblueprint/food4kids into templates-using-react-email
    
    * Lint Fix + Finalize Script + Generated email-tailwind-config that references design system index
    
    * format email templates
    
    * format and styling for all email temapltes
    
    * Update to email templates to ensure alignment with design
    
    * Replace variable field names in React Email templates
    
    * Export React Email templates into HTML
    
    * Linting, formatting, addressing claude's comments
    
    * Update gitignore to ignore files from email/html/static
    
    * Merge remote-tracking branch 'origin/main' into templates-using-react-email
    
    * Merge branch 'main' into templates-using-react-email

---
+++ frontend/emails/check-latest-announcement.tsx
@@ -0,0 +1,48 @@
+import { Button, Heading, Section, Text } from "@react-email/components";
+import * as React from "react";
+
+import F4KEmailLayout from "./components/F4KEmailLayout";
+
+export default function CheckLatestAnnouncementsEmail() {
+  return (
+    // REPLACE LINK HERE WITH ACTUAL URL (unless we want to Rick Roll recipients...)
+    <F4KEmailLayout previewText="New Announcement">
+
+      <Section className="px-[35px] pt-[10px] pb-[17px]">
+        <Heading className="text-[32px] font-nunito font-bold text-grey-500 mb-[22px]">
+          Check the latest announcement
+        </Heading>
+        
+        <Text className="text-[16px] leading-[24px] text-grey-500 m-0 mb-[24px]">
+            Hi Driver_Name_To_Replace,
+        </Text>
+        <Text className="text-[16px] text-grey-500 m-0 mb-[24px]">
+            There's a new announcement from the Food4Kids team:
+        </Text>
+
+        <Section className="border-l-[4px] border-solid border-grey-300 pl-[22px] mb-[48px]">
+          <Text className="text-[16px] text-grey-500 m-0">
+            <strong>Announcement_Name</strong>
+          </Text>
+          <Text className="text-[16px] text-grey-500 m-0">
+            Announcement_Body
+          </Text>
+        </Section>
+        <Text className="text-[16px] text-grey-500 m-0">
+            Please check the driver board to read the full update.
+        </Text>
+
+        <Section className="text-center mt-[32px] mb-[18px]">
+          <Button 
+          // REPLACE THIS WITH ACTUAL URL (unless we want to Rick Roll recipients...)
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
+          className="rounded-full border border-solid border-blue-400 bg-blue-300 px-[24px] py-[11px] font-nunito text-grey-100 text-[16px] tracking-wide"
+          >
+              View Announcement
+          </Button>
+        </Section>
+      </Section>
+      
+    </F4KEmailLayout>
+  );
+}
