bf570a20f4a720eb885c4486c8d6fd2fc77ea65a
Kerushani Sivaneswaran <99515111+Kerushani@users.noreply.github.com>
2026-08-07 08:02:10 -0400

    update down_revision

--- backend/python/migrations/versions/d4eefe397549_add_input_payload_error_message_and_.py
+++ backend/python/migrations/versions/d4eefe397549_add_input_payload_error_message_and_.py
@@ -1,7 +1,7 @@
 """add input payload, error message and summary columns to jobs
 
 Revision ID: d4eefe397549
-Revises: bc7876e56dd1
+Revises: a1c2e3f4b5d6
 Create Date: 2026-07-28 02:26:20.612153
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'd4eefe397549'
-down_revision = 'bc7876e56dd1'
+down_revision = 'a1c2e3f4b5d6'
 branch_labels = None
 depends_on = None
 
