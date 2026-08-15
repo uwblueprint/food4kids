d9b1a5ddbbe92efcee5b95d98b4b6e27a156ff74
Hy Lac <hylacnguyen2006@gmail.com>
2026-08-15 10:37:10 -0400

    match figma

--- frontend/src/features/calendar/CalendarWidget.tsx
+++ frontend/src/features/calendar/CalendarWidget.tsx
@@ -1,13 +1,28 @@
-import { Card, CardContent, Calendar } from '@/common/components';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Calendar,
+} from '@/common/components';
 
 export function CalendarWidget() {
   return (
-    <Card className="shadow-admin-bento col-span-2 rounded-4xl">
+    <Card className="shadow-admin-bento col-span-2 gap-4 rounded-4xl">
+      <CardHeader className="flex-row justify-between">
+        <div>
+          <h2 className="font-nunito text-h2 text-grey-500 font-bold">
+            Calendar
+          </h2>
+          <h3>This week</h3>
+        </div>
+        <div className="flex gap-4">
+          <Button variant="secondary">Print all routes</Button>
+        </div>
+      </CardHeader>
       <CardContent className="flex flex-col gap-4">
-        <h2 className="font-nunito text-h2 text-grey-500 font-bold">
-          Calendar
-        </h2>
-        <Calendar mode="single" />
+        <div></div>
+        <div></div>
       </CardContent>
     </Card>
   );
